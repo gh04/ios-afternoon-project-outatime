073ab0409bf341fdba298ea803251a6ef8b06a56
Gerardo Hernandez <gahernandez02@gmail.com>
2021-07-01 15:00:09 -0700

    Set up a programmatic view on TimeCircutsVC and set a date formatter.

--- OutaTime-II/OutaTime-II/TimeCircuitsViewController.swift
+++ OutaTime-II/OutaTime-II/TimeCircuitsViewController.swift
@@ -9,12 +9,46 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
 
+    // MARK: - Properties
+    var currentSpeed = 0
+    
+   // MARK: - Oulets
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presetTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    @IBOutlet weak var setTimeButton: UIButton!
+    @IBOutlet weak var travelBackButton: UIButton!
+    
+    
+    private var dateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateFormat =  "MMM d, yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
+   
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateViews()
     }
     
+    
+    // MARK: - IBActions
+    @IBAction func travelBackButton(_ sender: UIButton) {
+    }
+    
+    private func updateViews() {
+        presetTimeLabel.text = dateFormatter.string(from: Date())
+        speedLabel.text = "\(String(currentSpeed)) MPH"
+        lastTimeLabel.text = "--- -- ----"
+
+    }
 
     /*
     // MARK: - Navigation
